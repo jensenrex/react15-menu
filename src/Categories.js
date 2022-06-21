731d33442d1dfff12ee2138bc2b7de11ad6e606b
jensenrex <jensenrex@gmail.com>
2022-06-21 11:39:11 +1000

    create categories dynamically

--- src/Categories.js
+++ src/Categories.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ categories, filterItems }) => {
   
-  return <div className="btn-container">
-    <button className="filter-btn" onclick={() => filterItems('all')}>
-    all
-    </button>
-    <button className="filter-btn" onclick={() => filterItems('breakfast')}>
-    breakfast
-    </button>
-  </div>;
+  return (
+  <div className="btn-container">
+    {categories.map((category, index)=>{
+      return <button 
+        type="button" 
+        className="filter-btn" 
+        key={index}
+        onClick={()=>filterItems(category)}
+        >
+        {category}
+        
+      </button>
+
+    })}
+  </div>
+  );
 };
 
 export default Categories;
